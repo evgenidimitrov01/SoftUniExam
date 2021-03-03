27289cd22b0a11256b28ee307b535ba24049be9e
Evgeni Dimitrov <ev.dimitrov@abv.bg>
2021-03-03 10:57:43 +0200

    Add files via upload

--- Appium_Testing/Helpers.cs
+++ Appium_Testing/Helpers.cs
@@ -4,7 +4,7 @@ namespace Appium_Testing
     public static class Helpers
     {
         public static string appPackage = "contactbook.androidclient";
-        public static string appPath = @"F:\SoftUni_QA_Automation\21.02.2021_Exam\contactbook-androidclient.apk";
+        public static string appPath = "Replace Me";
         public static string appiumServerUrl = "http://[::1]:4723/wd/hub/";
         public const int WaitSecsForElements = 30;
     }
